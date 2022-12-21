48d75518d88c53c501304458dcac0ec95e7533d4
RAY0514 <116180895+RAY0514@users.noreply.github.com>
2022-12-21 17:56:40 +0800

    HW2

---
+++ HW02/src/VerifyResult.java
@@ -0,0 +1,38 @@
+public class VerifyResult {
+    private Boolean isVerifySuccess;
+    private String id;
+    private String message;
+
+    public Boolean getVerifySuccess() {
+        return isVerifySuccess;
+    }
+
+    public void setVerifySuccess(Boolean verifySuccess) {
+        isVerifySuccess = verifySuccess;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "content{" +
+                "isVerifySuccess =" + isVerifySuccess +
+                ", id ='" + id + '\'' +
+                ", message =" + message +
+                '}';
+    }
+}
