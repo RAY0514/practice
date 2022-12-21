7d663a6e2e15a821b2c2ce600bfb9291d3c0ce7f
RAY0514 <116180895+RAY0514@users.noreply.github.com>
2022-12-21 10:19:47 +0800

    test

--- HW/src/homework/verify/IdVerifyHelper.java
+++ HW/src/homework/verify/IdVerifyHelper.java
@@ -2,18 +2,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.*;
-
 public class IdVerifyHelper {
     static int[] stringToIntArray = new int[26];//放輸入的字串
     static int[] numberArray = {10, 11, 12, 13, 14, 15, 16, 17, 34, 18, 19, 20, 21, 22, 35, 23, 24, 25, 26, 27, 28, 29, 32, 30, 31, 32};//A-Z對應的數字
     static char[] characterArray = new char[26];//A-Z
-
     public static void main(String[] args) throws Exception {
         //step1 英文字母放進陣列
         insertCharacter();
         //step2 抓取檔案內容
-        File doc = new File("./src/idList.txt");//檔案路徑
-        BufferedReader obj = new BufferedReader(new FileReader(doc));
+        File file = new File("./src/idList.txt");//檔案路徑
+        BufferedReader obj = new BufferedReader(new FileReader(file));
         //step3 把內容放進list
         String string, line;
         List list = new ArrayList<>();
@@ -23,10 +21,10 @@ public class IdVerifyHelper {
         }
         // step4 檢查身分證是否正確
         for (int i = 0; i < list.size(); i++) {
-            String a = (String) list.get(i);
-            String b = a.toUpperCase();
+            String idNum = (String) list.get(i);
+            String upperIdNum = idNum.toUpperCase();
             System.out.println("===========您輸入的身分證字號" + list.get(i) + "=========");
-            if (check(b) == true) {
+            if (check(upperIdNum) == true) {
                 System.out.println("====驗證成功====");
             } else {
                 System.out.println("====驗證失敗====");
@@ -34,29 +32,28 @@ public class IdVerifyHelper {
         }
     }
 
-        static boolean check (String a){
-            int index = 0;
-            String inputString = a;//身分證字串放進來
-            Character firstLetter = inputString.charAt(0);//抓出字串第一個英文
-            for (int i = 1; i <= inputString.length() - 1; i++) {
-                stringToIntArray[i] = (int) (inputString.charAt(i)) - 48;//把身分證字號放入stringToIntArray[]，inputString.charAt(i)裡面的是字元1，(int)(inputString.charAt(i))傳回ASCII，0的ASCII十進位是48所以抓到什麼數字轉成int -48
-            }
-            index = Arrays.binarySearch(characterArray, firstLetter);//找英文字母在characterArray中的第幾個index//Arrays.binarySearch(要搜索的數組,要索的值)
-            int d0 = numberArray[index];//英文對應到的數字抓出來放入d0
-            int x2 = (d0) % 10;//d0的個位數//除10取餘數
-            int x1 = (int) Math.floor((d0) / 10);//d0的十位數//Math.floor取整數
-            int result = (x1 * 1) + (9 * x2) + (8 * stringToIntArray[1]) + (7 * stringToIntArray[2]) + (6 * stringToIntArray[3]) + (5 * stringToIntArray[4]) + (4 * stringToIntArray[5]) + (3 * stringToIntArray[6]) + (2 * stringToIntArray[7]) + (stringToIntArray[8]);
-            int checkCode = 10 - (result % 10);
-            if (stringToIntArray[9] == checkCode || stringToIntArray[9]==0) { //stringToIntArray[9]身分證的最後一碼為檢查碼
-                return true;
-            }
-             return false;
+    public static boolean check (String id){
+        int index = 0;
+        String inputString = id;//身分證字串放進來
+        Character firstLetter = inputString.charAt(0);//抓出字串第一個英文
+        for (int i = 1; i <= inputString.length() - 1; i++) {
+            stringToIntArray[i] = (int) (inputString.charAt(i)) - 48;//把身分證字號放入stringToIntArray[]，inputString.charAt(i)裡面的是字元1，(int)(inputString.charAt(i))傳回ASCII，0的ASCII十進位是48所以抓到什麼數字轉成int -48
         }
-
-        static void insertCharacter() {
-            for (int i = 0; i <= 25; i++) {
-                characterArray[i] = (char) (65 + i);//把英文字母放入字元陣列，強制轉型A->65
-            }
+        index = Arrays.binarySearch(characterArray, firstLetter);//找英文字母在characterArray中的第幾個index//Arrays.binarySearch(要搜索的數組,要索的值)
+        int d0 = numberArray[index];//英文對應到的數字抓出來放入d0
+        int x2 = (d0) % 10;//d0的個位數//除10取餘數
+        int x1 = (int) Math.floor((d0) / 10);//d0的十位數//Math.floor取整數
+        int result = (x1 * 1) + (9 * x2) + (8 * stringToIntArray[1]) + (7 * stringToIntArray[2]) + (6 * stringToIntArray[3]) + (5 * stringToIntArray[4]) + (4 * stringToIntArray[5]) + (3 * stringToIntArray[6]) + (2 * stringToIntArray[7]) + (stringToIntArray[8]);
+        int checkCode = 10 - (result % 10);
+        if (stringToIntArray[9] == checkCode || stringToIntArray[9]==0) { //stringToIntArray[9]身分證的最後一碼為檢查碼
+            return true;
         }
-
+         return false;
     }
+
+    public static void insertCharacter() {
+        for (int i = 0; i <= 25; i++) {
+            characterArray[i] = (char) (65 + i);//把英文字母放入字元陣列，強制轉型A->65
+        }
+    }
+}
