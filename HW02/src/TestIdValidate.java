2e81f33f6e7083fe6495cfc669a5785a7c2c3a2a
RAY0514 <116180895+RAY0514@users.noreply.github.com>
2022-12-22 10:28:48 +0800

    HW2

--- HW02/src/TestIdValidate.java
+++ HW02/src/TestIdValidate.java
@@ -1,6 +1,8 @@
 public class TestIdValidate {
+
     public static void main(String[] args) throws Exception {
         IdVerifyHelper idVerifyHelper=new IdVerifyHelper("./src/idList.txt");
         System.out.println(idVerifyHelper.validate("./src/idList2.txt"));
     }
+
 }
