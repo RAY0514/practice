c86e5ed705c1ce239b31666f693cec642dde641a
RAY0514 <116180895+RAY0514@users.noreply.github.com>
2022-12-27 17:18:16 +0800

    HW05

---
+++ HW05/demo-1/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class Student extends Member {
+    String classes;
+    String admissionYearMonth;
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    public void setAdmissionYearMonth(String admissionYearMonth) {
+        this.admissionYearMonth = admissionYearMonth;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public String getAdmissionYearMonth() {
+        return admissionYearMonth;
+    }
+}
